94f58ff9ab985b7b63fedbc0d357f40ce6d77b9f
dcmbrk <ldtung41@gmail.com>
2025-12-14 19:51:32 +0700

    add reader for extract pdf data

---
+++ main.py
@@ -0,0 +1,21 @@
+import sys
+from reader import Reader
+
+def main():
+    if len(sys.argv) == 4:
+        reader = Reader(path=sys.argv[2], type=sys.argv[3])
+
+        if sys.argv[1] == '-file':
+            print("EXTRACTING FILE....")
+            reader.extract_file()
+            print("DONE....")
+            
+        elif sys.argv[1] == '-folder':
+            print("EXTRACTING FOLDER....")
+            reader.extract_folder()
+            print("DONE....")
+        
+
+if __name__ == "__main__":
+    main()
+
---
+++ reader.py
@@ -0,0 +1,67 @@
+from pypdf import PdfReader
+import re
+from sqlalchemy import select
+from connection.db import Session
+from connection.models import Rules, Data
+import os
+
+class Reader():
+    def __init__(self, path, type):        
+        self.path = path
+        self.type = type
+
+
+    def extract_file(self):
+        reader = PdfReader(self.path)
+        text = reader.pages[0].extract_text()
+
+        with Session() as session:
+            with session.begin():
+                q = select(Rules)   
+                r = session.scalars(q).all()
+
+                for rule in r:
+                    if rule.type == self.type:
+
+                        data = Data(
+                            name = os.path.basename(self.path),
+                            rule_id = rule.id,
+                            value = self.get_value(text, rule.regex)
+                        )
+
+                        session.add(data)
+
+                        break
+
+
+    def extract_folder(self):
+        for file in os.listdir(self.path):
+            if file.endswith('.pdf'):
+                file_path = os.path.join(self.path, file)
+
+                reader = PdfReader(file_path)
+                page = reader.pages[0]
+                text = page.extract_text()
+
+                with Session() as session:
+                    with session.begin():
+
+                        for rule in session.scalars(select(Rules)).all():
+                            if rule.type == self.type:
+                                
+                                data = Data(
+                                    name = os.path.basename(file_path),
+                                    rule_id = rule.id,
+                                    value = self.get_value(text, rule.regex)
+                                )
+
+                                session.add(data)
+                                
+                                break
+
+    def get_value(self, text, regex):
+        res = {}
+        for field, match in regex.items():
+            res[field] = re.search(match, text).group()   
+        return res
+
