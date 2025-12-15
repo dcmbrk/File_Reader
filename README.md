6a5ee9d5e5d97429c788c6c54d3613809d721ef3
LE ANH SON <1771020591@dnu.edu.vn>
2025-12-15 09:12:56 +0700

    Add files

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # PDF READER
-### Cai dat thu vien:
+### Cài đặt thư viện
 `pip install -r requirements.txt`
-### Cai dat pgadmin, postgrest (neu can)
+### Cài đặt pgadmin, postgrest (nếu cần)
 `docker-compose up -d`
-### Cai dat ket noi database (SQLAlchemy) trong .env
+### Cài đặt kết nối databasae (SQLAlchemy) trong .env
 `DATABASE_URL='postgresql+psycopg2://Username:Password@localhost:5432/database'`
-## Trich xuat thong tin tu file
+## Trích xuất thông tin từ file
 `python main.py -file <file-path> <file-type>`
-## Trich xuat thong tin tu folder
+## Trích xuất thông tin từ folder
 `python main.py -folder <folder-path> <file-type>`
--- import_data.py
+++ import_data.py
@@ -2,20 +2,22 @@ import json
 from connection.db import Model, Session, engine
 from connection.models import Rules
 
+
 def main():
     Model.metadata.drop_all(engine)
     Model.metadata.create_all(engine)
 
     with Session() as session:
         with session.begin():
-            with open('data/rules.json', 'r', encoding='utf-8') as f:
+            with open("data/rules.json", "r", encoding="utf-8") as f:
                 data = json.load(f)
-                
+
                 data = [data]
 
                 for item in data:
-                    rule = Rules(**item) 
+                    rule = Rules(**item)
                     session.add(rule)
+
 
 if __name__ == "__main__":
     main()
